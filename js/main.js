375d06b829a080a81b876e8e5d7ee2c6f20baf7c
Flavio <flavio.alessandri@gmail.com>
2021-02-03 16:36:03 +0100

    intro animation

--- js/main.js
+++ js/main.js
@@ -1,120 +1,69 @@
 function addListenerMulti(element, eventNames, listener) {
   var events = eventNames.split(' ');
-  for (var i=0, iLen=events.length; i<iLen; i++) {
-  element.addEventListener(events[i], listener, false);
+  for (var i = 0, iLen = events.length; i < iLen; i++) {
+    element.addEventListener(events[i], listener, false);
   }
 }
 
-function wheelSmartphone(ev){  
-  var popups = document.querySelectorAll('div.popup');
-  var markerPositionY = document.querySelector('.marker').getBoundingClientRect().y;
-  var markerPositionBottom = document.querySelector('.marker').getBoundingClientRect().y;  
-  if(markerPositionY >-2000 || markerPositionBottom >-2000 ) { 
-    [].forEach.call(popups, (popup) => popup.classList.remove('fadeIn'));
-  }
-  else {
-     [].forEach.call(popups, (popup) => popup.classList.add('fadeIn'));  
-    }    
-}
-
-function wheel(event) {  
+function wheel(event) {
   var popups = document.querySelectorAll('div.popup');
   var markerPositionY = document.querySelector('.marker').getBoundingClientRect().y;
   var markerPositionBottom = document.querySelector('.marker').getBoundingClientRect().y;
 
-  if(markerPositionY >-2000 || markerPositionBottom >-2000 ) {
-     [].forEach.call(popups, (popup) => popup.classList.remove('fadeIn'));
-    }
-  else {
-     [].forEach.call(popups, (popup) => popup.classList.add('fadeIn'));  
-    }
+  if (markerPositionY <= -1000 || markerPositionBottom <= -1000) {
+    [].forEach.call(popups, (popup) => popup.classList.add('fadeIn'));
+  }
 
-    var delta = 0;
-    if (event.wheelDelta) { delta = event.wheelDelta / 120;} //metodo che utilizza DOMMouseScroll su browser CHROME
-    else if (event.detail)  { delta = -event.detail / 3;  }//metodo che utilizza DOMMouseScroll su browser MOZILLA
-  
-    handle(delta); 
-    
-    if (event.preventDefault) { ()=>  event.preventDefault() } //utilizzando questo sistema prevengo il comportamento di default del Browser   
+  if (event.preventDefault) {
+    () => event.preventDefault()
+  }  
 }
 
-function handle(delta) {  
-    var time = 1000;
-    var distance =950;   
 
-    $(document.documentElement).stop().animate({
-      scrollTop: $(window).scrollTop() - (distance * delta)
-  }, time );
-}
 
-function mobileChk(){
+function mobileChk() {
   let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  (function (a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
 
-function animateLeaves(elem){
+function animateLeaves(elem) {
 
-  if(window.innerHeight<500){ elem.forEach( elemDim  => elemDim.style.width= "23px"); }
-
-  if(window.innerWidth>1000){ document.getElementById('leaf').style.left= "55%"; document.getElementById('leaf2').style.left= "45%" }
-
-  
-  
-  elem.forEach( rotating  => {
-    rotating.classList.add('rotation') 
-    setTimeout (() => {
+  elem.forEach(rotating => {
+    rotating.classList.add('rotation')
+    setTimeout(() => {
       rotating.classList.remove('rotation');
-    },1000);
+    }, 1000);
 
-    setTimeout (() => {      
-      rotating.classList.remove('popup');    
-    },5000);
-      
+    setTimeout(() => {
+      rotating.classList.remove('popup');
+    }, 5000);
 
   })
-//  rotate.classList.add('rotation');
-//   setTimeout (() => {rotate.classList.remove('rotation')},1000)
 }
 
-function init(){ 
 
+function init() {
 
-
-  var rotate = document.querySelectorAll('.rotate');  
-
+  var rotate = document.querySelectorAll('.rotate');
   var animation = bodymovin.loadAnimation({
-    container : document.getElementById("animation"),
+    container: document.getElementById("animation"),
     render: 'svg',
     loop: false,
-    autoplay:false,
+    autoplay: false,
     path: "./src/data.json"
   });
 
-  $(window).scroll( () => { animation.play(); animateLeaves(rotate)});
+  $(window).scroll((ev) => {
+    console.log("SCROLL");
+    animation.play();
+    animateLeaves(rotate);
+    wheel(ev)
+  });
 
-  //-------------------------------------------------------------------------------------------------
-  var supportsPassive = false;
-  try {
-    var opts = Object.defineProperty({}, 'passive', {
-      get: function() {
-        supportsPassive = true;
-      }
-    });    
-    window.addEventListener("testPassive", null, opts);
-    window.removeEventListener("testPassive", null, opts);
-  } catch (e) {}  
-  //-------------------------------------------------------------------------------------------------
-  
-//verifica se si sta navigando su mobile o su desktop 
-if(mobileChk()===false){ //------------------------------se Desktop
-  window.addEventListener('DOMMouseScroll', wheel, supportsPassive ? true : false);  
-}else{   //----------------------------------------------se Mobile
-  window.addEventListener('touchstart', wheelSmartphone, supportsPassive ? true : false);
-} 
 
 }
 
 $(document).ready(init);
-
-
